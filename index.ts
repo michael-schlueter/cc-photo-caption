928d4e9de8333d168d59de6d3eecb8ec6ed345c4
Michael Schlueter <schluet0r4life@gmail.com>
2022-10-24 11:11:30 +0200

    setup express

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+const logger = require("morgan");
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
