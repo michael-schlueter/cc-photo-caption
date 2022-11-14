e5b95d5d731a20d21c11e2e39c873dda92839f6d
Michael Schlueter <schluet0r4life@gmail.com>
2022-11-14 15:19:28 +0100

    fix user type in user service

--- services/users.services.ts
+++ services/users.services.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 const bcrypt = require("bcrypt");
 
+interface INewUser {
+  email: string;
+  password: string;
+}
+
 const prisma = new PrismaClient();
 
 export const findUserByEmail = (email: string) => {
@@ -11,8 +16,7 @@ export const findUserByEmail = (email: string) => {
   });
 };
 
-// @ts-ignore
-export const createUserByEmailAndPassword = (user) => {
+export const createUserByEmailAndPassword = (user: INewUser) => {
   user.password = bcrypt.hashSync(user.password, 12);
   return prisma.user.create({
     data: user,
